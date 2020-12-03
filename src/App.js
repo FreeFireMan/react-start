01838be1f3a2ab5b7237e05dbaa57711d7f543c6
SashaKruts <disaster1209>
2020-12-03 19:07:56 +0200

    lesson1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {users} from "./database/usersBase/userBase";
+import UserComponent from "./components/user/UserComponent";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <div>{
+                users.map((user,index) =>{
+                    let clsX = index%2 ? 'two' : 'one'
+
+                     return(
+                         <UserComponent
+                     item={user}
+                     cls={clsX}
+                     key={user.id}/>
+                     )
+                })
+            }
+            </div>
+        );
+    }
 }
 
 export default App;
