b59040ef7eac02633290f660d248ef3dd20dd4a2
freefireman <beluh.c.s@gmail.com>
2020-12-21 19:28:58 +0200

    little fix))

--- src/App.js
+++ src/App.js
@@ -1,41 +1,54 @@
 import React, {Component} from 'react';
+
 class App extends Component {
 
-	inputMain = React.createRef();
-
-	state = {inputValue: '',users: [], choseOne: null}
-
-	componentDidMount() {
-		fetch('https://jsonplaceholder.typicode.com/users')
-			.then(value => value.json)
-			.then(value => {
-				this.setState({users: value})
-			})
-	}
-
-	oninputFill = () =>{
-		this.setState({inputValue: this.inputMain.current.value});
+    state = {
+        inputValue: '',
+        users: [],
+        isLoading: false,
+        choseOne: null
     }
 
-    onFormSubmit = (e) =>{
-	    e.preventDefault();
+    componentDidMount() {
+        this.setState({isLoading: true});
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(value => value.json())
+            .then(users => {
+                this.setState({users, isLoading: false})
+            })
     }
 
-    userFind = (id) => {this.setState({choseOne: this.state.users.find(value => value.id === id)})}
+    oninputFill = ({target: {value}}) => {
+        this.setState({inputValue: value});
+    }
 
-	render() {
-		let {inputValue,choseOne} = this.state;
-		let {userFind,item} = this.props
-		return (
-			<div>
-				<form onSubmit={this.onFormSubmit}>
-					<input ref={this.inputMain} type={'number'} onInput={this.oninputFill} value={inputValue}/>
-					<button onClick={()=> userFind(item.id)}>send</button>
-				</form>
-				{choseOne && <h2>{choseOne.id}-{choseOne.name}</h2>}
-			</div>
-		);
-	}
+    userFind = () => {
+        const {inputValue, users} = this.state;
+		const choseOne = users.find(value => value.id === +inputValue);
+		choseOne
+			? this.setState({choseOne: `${choseOne.id}-${choseOne.name}`})
+			: this.setState({choseOne: 'User not found'})
+
+    }
+
+    render() {
+
+        let {inputValue, choseOne, isLoading} = this.state;
+
+        return (
+            <div>
+                {isLoading
+                    ? <h1>LOADING.....</h1>
+                    : <div>
+                        <input type='number' onInput={this.oninputFill} value={inputValue}/>
+                        <button onClick={this.userFind}>send</button>
+                      </div>
+                }
+
+                <h2>{choseOne}</h2>
+            </div>
+        );
+    }
 }
 
 export default App;
