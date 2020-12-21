236e7c176328ea724973417e6870e30c4b338f62
SashaKruts <disaster1209>
2020-12-21 19:10:21 +0200

    lesson6

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import React, {Component} from 'react';
-import {users} from "./database/usersBase/userBase";
-import UserComponent from "./components/user/UserComponent";
-
 class App extends Component {
-    render() {
-        return (
-            <div>{
-                users.map((user,index) =>{
-                    let clsX = index%2 ? 'two' : 'one'
 
-                     return(
-                         <UserComponent
-                     item={user}
-                     cls={clsX}
-                     key={user.id}/>
-                     )
-                })
-            }
-            </div>
-        );
+	inputMain = React.createRef();
+
+	state = {inputValue: '',users: [], choseOne: null}
+
+	componentDidMount() {
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then(value => value.json)
+			.then(value => {
+				this.setState({users: value})
+			})
+	}
+
+	oninputFill = () =>{
+		this.setState({inputValue: this.inputMain.current.value});
     }
+
+    onFormSubmit = (e) =>{
+	    e.preventDefault();
+    }
+
+    userFind = (id) => {this.setState({choseOne: this.state.users.find(value => value.id === id)})}
+
+	render() {
+		let {inputValue,choseOne} = this.state;
+		let {userFind,item} = this.props
+		return (
+			<div>
+				<form onSubmit={this.onFormSubmit}>
+					<input ref={this.inputMain} type={'number'} onInput={this.oninputFill} value={inputValue}/>
+					<button onClick={()=> userFind(item.id)}>send</button>
+				</form>
+				{choseOne && <h2>{choseOne.id}-{choseOne.name}</h2>}
+			</div>
+		);
+	}
 }
 
 export default App;
